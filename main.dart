c2e6d6d46e80a9e380df2050bf781289546f0dc3
phalkandari <mohammad.kandary@gmail.com>
2022-11-09 19:17:06 +0300

    Finished 2 parts

--- main.dart
+++ main.dart
@@ -1,6 +1,32 @@
-void main(){
-  
-  
-  
-  
+void main() {
+// Menu
+  Map<String, dynamic> menu = {
+    'burger': 6.5,
+    'pizza': 5,
+    'water': 1.5,
+  };
+  print(menu);
+
+  menu['chocolate cake'] = 3;
+  print(menu);
+
+  menu['water'] = 0.75;
+  print(menu);
+
+  print('Menu');
+  print('');
+  for (var meal in menu.entries) {
+    print('${meal.key} -> ${meal.value}KD');
+  }
+
+// Calculate Orders
+  List<String> order = ['pizza', 'water'];
+
+  double total = 0;
+  for (var meal in order) {
+    total += menu[meal];
+  }
+  print('KD${total}');
+
+// Unavailable Items
 }
